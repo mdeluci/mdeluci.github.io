91438e3a582fc5dcb2aa273ae440614e838a5353
mdeluci <mdeluci@purdue.edu>
2024-10-03 20:14:38 +0000

    Deploying to gh-pages from @ mdeluci/mdeluci.github.io@43b7a59fc3484834e4bbc53b7ec012be82c89da8 🚀

--- Melosh/test_center.py
+++
@@ -1,7 +0,0 @@
-from manim import *
-
-class CenteredText(Scene):
-    def construct(self):
-        title = Tex(r'Centered Text').move_to(ORIGIN)
-        self.play(Write(title))
-        self.wait()
